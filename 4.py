103034f016e90a6b2ee9699cc111ad33632cfdf1
Albert12-alt <albertkhanigan@gmail.com>
2022-12-16 21:53:41 +0100

    dz 3

---
+++ 4.py
@@ -0,0 +1,44 @@
+class Class():
+
+ def __init__(self, name):
+    self.name = name
+    self.points = 20
+    self.hp = 100
+def handKick(self):
+    pass
+
+class knight():
+
+ def __int__(self , name):
+    self.name = name
+    self.hp = 150
+ def  swordAttack(self):
+        self.sword_attack = 20
+ def  shieldAttack(self):
+        self.shield_attack = 10
+ def  evasion(self):
+        self.save_hp = 140
+
+class  Magus():
+
+ def __int__(self , name):
+    self.name = name
+    self.hp = 200
+ def  fireballAttack(self):
+        self.fireball_attack = 40
+ def  teleport(self):
+        self.save_hp = 200
+
+class  Archer():
+
+ def __int__(self , name):
+    self.name = name
+    self.hp = 130
+ def  ArcheryAttack(self):
+        self.fireball_attack = 15
+ def  Secrecy(self):
+    self.name = None
+
+K = knight(1)
+M = Magus(2)
+A = Archer(3)
