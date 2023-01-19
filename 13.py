93c4ad95d44c70606f0ae2c606f45db18f7f0d1f
Albert12-alt <albertkhanigan@gmail.com>
2023-01-19 19:34:29 +0100

    lol

---
+++ 13.py
@@ -0,0 +1,27 @@
+import requests
+raise ValueError
+
+
+l = [0, 1, 2, 3, 4, 5, 6]
+
+a = 2
+
+b = 3
+
+
+
+requests.get("http://localhost:1")
+raise AttributeError
+a = a + b * a
+
+a = a * b / l[10]
+
+b = a * len(l)
+
+b = b / l[0]
+
+
+
+print(a, b)
+ValueError
+    
---
+++ 14.py
@@ -0,0 +1,38 @@
+import random
+
+
+
+
+def name_randomizer():
+
+    names = ["oleg", "dmitro", "kirill", "sasha"]
+
+    surnames = ["kirilow", "sergeev", "serpentin", "arentir", "legenda"]
+
+    while True:
+        #! Получаем случайное имя и возвращаем его
+
+        pass
+
+
+
+
+rand = name_randomizer()
+
+print(next(rand))
+
+print(next(rand))
+
+print(next(rand))
+
+print(next(rand))
+
+
+
+
+
+
+
+surnames = "aprotokol , zuza , vityavich , lolylovisch , genderev"
+
+names = "petya , oleg , lucka , lila , sergey"
