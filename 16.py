62ec0880d9eb85c190d68b462d6f38027b04d314
Albert12-alt <albertkhanigan@gmail.com>
2023-01-21 17:12:36 +0100

    last one

---
+++ 16.py
@@ -0,0 +1,122 @@
+class Car():
+ def __init__(self, name , color , company , turn , max_speed , fuel , max_fuel , min_speed , angle , fuel_consumptio , x , y , speed , price , stop , motor , wheels):
+        self.name = name
+        self.color = color
+        self.company = company
+        self.max_speed = max_speed
+        self.fuel = fuel
+        self.max_fuel = max_fuel 
+        self.min = min_speed
+        self.fuel_consumptio = fuel_consumptio
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.price = price  
+        self.stop = stop
+        self.motor = motor
+        self.wheels = wheels
+        self.turn = turn
+        self.motor = motor 
+        self.angle = angle
+
+def getname(self):
+     return self.name + "  " + self.company
+
+def fuel(self):
+    if self.max_fuel == 100:
+        return True
+    if self.fuel < 100:
+        self.fuel = 100
+    return self.__fuel__ + "/" + self.max_fuel
+
+def ride(self):
+    self.max_speed = 400
+    if self.speed > 400:
+        self.speed =  400
+    if self.isride:
+        self.__fuel__ -= 0.9
+    if self.fuel == 0:
+       self.isride = False
+
+
+def S(self):
+    return self.x * self.y
+
+def brake(self):
+    self.fuel -= 0.2
+    self.speed -= 5
+    if self.speed < 0:
+        True
+    
+
+def check_fuel(self):
+    if self.__fuel__ == 0 and self.isride:
+        return False
+    return True
+
+def stop(self):
+    if self.speed == 0 and self.isride:
+        self.isride = False
+        self.wheels = False
+
+
+def motor(self):
+    if self.speed < 0 and self.isride:
+        True    
+    
+def wheels(self):
+    if self.speed < 0 and self.isride:
+        self.wheels = True
+
+def self(angle):
+  if self.speed < 0:
+    self.new(angle)
+
+
+name = "F8_TRIBUTO"
+
+company = "Ferrari"
+
+motor = "V8"
+
+price = "350000"
+
+
+
+class Motor:
+
+    def __init__(ride , self , name , speed):
+        self.name = name
+        self.ride = ride
+        self.speed = speed
+
+def ride(self):
+    self.max_speed = 400
+    if self.speed > 400:
+        self.speed =  400
+    if self.isride:
+        self.__fuel__ -= 0.9
+    if self.fuel == 0:
+       self.isride = False 
+
+def motor(self):
+    if self.speed < 0 and self.isride:
+        True 
+    if self.speed < 0 and self.isride:
+          self.wheels = True  
+
+class Wheel:
+    def __init__(self, name):
+        self.name = name
+
+class Wheel:
+    def __init__(self, name):
+        self.name = name
+
+class Wheel:
+    def __init__(self, name):
+        self.name = name
+
+class Wheel:
+    def __init__(self, name):
+        self.name = name
